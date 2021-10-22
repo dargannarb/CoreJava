8c6b7b503709ac1000bdaeb51f0eb56819183cf3
Kapati Thulasiram <kapthula@publicisgroupe.net>
2021-10-22 15:31:36 +0530

    first commit

--- java_Basics/src/oops/Polymarphisim.java
+++ java_Basics/src/oops/Polymarphisim.java
@@ -1,5 +1,18 @@
 package oops;
 
+import oppsExaplanation.ExampleOverLoading;
+
 public class Polymarphisim {
 
+	
+	// to show one object in many form
+	// 1. compiled polymorphism , 2 runtime plomorphism
+	// method overloading , method overriding
+	
+	public static void main(String[] args) {
+		ExampleOverLoading.add(2l,3l);
+	}
+	
+	
+	
 }
--- java_Basics/src/programms/Ladies.java
+++ java_Basics/src/programms/Ladies.java
@@ -2,4 +2,6 @@ package programms;
 
 public  class Ladies {
 	
+	
+	
 }
